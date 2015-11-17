ccc1bf061c1269cfb75a79bac0c9cc048143f48c
d <dhenton@localhost.com>
2015-11-17 17:39:55 -0600

    working on data structure

--- public_html/mountebank-UI/app/app.js
+++ public_html/mountebank-UI/app/app.js
@@ -1,11 +1,11 @@
 angular.module('myApp', ['ngRoute', 'app.homePages'])
 
-  .constant('TPL_PATH', '/mountebank-UI/mountebank-UI/templates')
+  .constant('TPL_PATH', '/mountebank-UI/mountebank-UI/templates/')
 
   .config(function($routeProvider, TPL_PATH) {
     $routeProvider.when('/',{
       controller : 'HomeCtrl',
       controllerAs: 'home',
-      templateUrl : TPL_PATH + '/home.tpl.html'
+      templateUrl : TPL_PATH + 'home.tpl.html'
     });
   });
---
+++ public_html/mountebank-UI/sections/homePage.ctl.js
@@ -0,0 +1,55 @@
+
+
+angular.module('app.homePages', [])
+
+
+
+        .controller('HomeCtrl', function ($scope, $log  ) {
+            vm = this;
+            vm.errorMessage = "No errors";
+            vm.data =
+                    {
+                        "id": "alpha",
+                        "items": [
+                            {"name": "Fred", "age": 34},
+                            {"name": "John", "age": 12},
+                            {"name": "Lorenzo", "age": 24},
+                            {"name": "Betgh", "age": 76},
+                            {"name": "Mary", "age": 26}
+                        ]
+
+                    }
+
+
+            vm.inputChange = function (angularForm,ev)
+            {
+               //  $log.debug("inputChange "+ev)  ; 
+                 
+               //  vm.data.items[idx] = angularForm.item
+               vm.displayData = angular.toJson(vm.data, true);
+                
+            }
+
+
+
+            vm.displayData = angular.toJson(vm.data, true);
+
+            $scope.$watch(
+                    function watchDisplayData(scope) {
+                        // Return the "result" of the watch expression.
+                        return(vm.displayData);
+                    },
+                    function handleDisplayChange(newValueStr, oldValueStr) {
+                        try
+                        {
+                            vm.data = angular.fromJson(newValueStr);
+                            vm.errorMessage =  "No errors";
+                        }
+                        catch (err)
+                        {
+                            vm.errorMessage = err.message;
+                        }
+                    }
+            );
+
+        });
