60e04500678e6051c160f9dcc8019de57b63b4cd
d <dhenton@localhost.com>
2015-11-18 13:34:35 -0600

    tests to confirm twitter only page changesworking on transforming mountebank form 3

--- public_html/mountebank-UI/sections/homePage.ctl.js
+++ public_html/mountebank-UI/sections/homePage.ctl.js
@@ -8,35 +8,57 @@ angular.module('myApp')
             vm.data = ImpostersService.getData("1");
             vm.currentIndex = 0;
             vm.mounteBankUrl = "http://localhost:4545"
-   
+            vm.currentImposter = null;
+            vm.responseBuffer = {};
+            vm.responseBuffer.headerCount = [1,2,3,4,5];
+
+            //change which imposter you are on
             vm.changeImposter = function (idx)
             {
-               // $log.debug(idx);
+                // $log.debug(idx);
                 vm.currentIndex = idx;
-                vm.currentData = vm.data.imposters[vm.currentIndex];
-               
+                vm.currentImposter = vm.data.imposters[vm.currentIndex];
+                scatter();
+
+
 
             }
-            vm.changeImposter(vm.currentIndex);
- 
-            vm.inputChange =function(item,event)
+
+            function scatter()
             {
-                
+                vm.responseBuffer.status  = vm.currentImposter.response.status;
+                vm.responseBuffer.headers = vm.currentImposter.response.headers;
+                vm.responseBuffer.body    =  angular.toJson(vm.currentImposter.response.body,true);
+
+            }
+            //initial call
+            vm.changeImposter(vm.currentIndex);
+
+            vm.inputChange = function (item, event)
+            {
+                //console.log(angular.toJson(item))
             }
 
 
-//            vm.displayData = angular.toJson(vm.data, true);
-//
+            vm.tabSelect = function (x)
+            {
+                $log.debug("tab " + x);
+                vm.displayData = angular.toJson(vm.data, true);
+            }
+
+
+            vm.displayData = angular.toJson(vm.data, true);
+
 //            $scope.$watch(
 //                    function watchDisplayData(scope) {
 //                        // Return the "result" of the watch expression.
-//                        return(vm.displayData);
+//                        return(vm.data);
 //                    },
 //                    function handleDisplayChange(newValueStr, oldValueStr) {
 //                        try
 //                        {
-//                            vm.data = angular.fromJson(newValueStr);
-//                           // $log.debug("SMMDKFJLDFJDLFJDLJ")
+//                            vm.displayData = angular.toJson(newValueStr,true);
+//                             $log.debug("called change ")
 //                            vm.errorMessage =  "No errors";
 //                        }
 //                        catch (err)
