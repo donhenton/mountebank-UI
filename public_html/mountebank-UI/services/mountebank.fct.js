b84f4677f407e41cc37f3eb67b52bccaf67ef601
d <dhenton@localhost.com>
2015-11-21 17:19:53 -0600

    working on mountebank translation 2

--- public_html/mountebank-UI/services/mountebank.fct.js
+++ public_html/mountebank-UI/services/mountebank.fct.js
@@ -15,8 +15,126 @@ function mountebankService($log)
      */
     function translate(data)
     {
-        return "get a job";
+
+        var translated = {};
+        translated.port = data.port;
+        translated.protocol = "http";
+        translated.stubs = [];
+        angular.forEach(data.imposters, function (imposter, idx)
+        {
+            var newStub = {"responses": [], "predicates": []}
+            translated.stubs.push(newStub);
+            //responses
+            angular.forEach(imposter.responses, function (response, idx)
+            {
+                var headerVar = {};
+                angular.forEach(response.headers, function (header, idx)
+                {
+                    headerVar[header.key] = header.value;
+                });
+
+                var newResponse = {
+                    "is": {
+                        "statusCode": response.status,
+                        "headers": headerVar,
+                        "body": response.body
+
+
+                    }
+                };
+                newStub.responses.push(newResponse);
+            })
+
+            // predicates
+            var newPredicate = {};
+            var matchHeaders = {};
+            angular.forEach(imposter.match.headers, function (header, idx)
+            {
+                matchHeaders[header.key] = header.value;
+            });
+            newPredicate[imposter.match.body_match.type] =
+                    {
+                        "path": imposter.match.path,
+                        "body": imposter.match.body_match.body,
+                        "method": imposter.match.verb,
+                        "headers": matchHeaders
+
+
+
+                    }
+            newStub.predicates.push(newPredicate);
+
+        });
+
+
+        return angular.toJson(translated, true);
     }
 
     return exports;
 }
+
+/*
+ 
+ 
+ 
+ 
+ {
+ "port": 7777,
+ "protocol": "http",
+ "stubs": [{
+ "responses": [
+ {
+ "is": {
+ "statusCode": 200,
+ "headers":
+ {
+ "bonzo": "dog"
+ },
+ "body": "{\"answer\": \"get a job\"}"
+ 
+ 
+ }
+ },
+ {
+ "is": {
+ "statusCode": 200,
+ "headers":
+ {
+ "bonzo": "dog"
+ },
+ "body": "{\"answer\": \"get another job\"}"
+ 
+ 
+ }
+ }
+ ],
+ "predicates": [{
+ "and": [
+ {
+ "equals": {
+ "path": "/test",
+ "body":"{\"alpha\": 25}",
+ "method": "POST",
+ "headers": {
+ "Content-Type": "application/json"
+ }
+ }
+ } 
+ ]
+ }]
+ }]
+ }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ */
