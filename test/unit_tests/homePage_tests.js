88be01702f73ed44ef68d598eb89ec9377ed7ea9
d <dhenton@localhost.com>
2015-11-27 11:19:14 -0600

    working control injection test

--- test/unit_tests/homePage_tests.js
+++ test/unit_tests/homePage_tests.js
@@ -1,27 +1,105 @@
+/* global expect */
+
 describe('Home Pages', function () {
 
-    beforeEach(module('myApp'));
+    var testCollection = {
+        "port": 9999,
+        "id": 1,
+        "description": "Test Collection",
+        "imposters": [
+            {
+                "responses": [
+                    {
+                        "status": 200,
+                        "headers": [],
+                        "body": "bonzo"
+                    }
+                ],
+                "match": {
+                    "path_match": {
+                        "type": "equals",
+                        "value": "path"
+                    },
+                    "verb": "GET",
+                    "headers": [],
+                    "query_params": [],
+                    "body_match": {
+                        "type": "equals",
+                        "body": "body"
+                    }
+                }
+            }]
+    };
 
-    it('should test the homePages controller', inject(function ($controller, $rootScope) {
+    var headerLocation = null;
+
+    beforeEach(function ()
+    {
+        module('myApp');
+        module(function ($provide) {
+
+            function currentImposterFunction()
+            {
+
+//                function getCurrentImposter()
+//                {
+//                    return {};
+//                }
+//                var exports = {"getCurrentImposter": getCurrentImposter};
+//
+//
+//                return exports;
+                return testCollection;
+            }
+
+            function collectionItemsFunction()
+            {
+                return {};
+            }
+
+
+            $provide.factory('currentImposter', [currentImposterFunction]);
+            $provide.factory('collectionItems', [collectionItemsFunction]);
+        });
+
+    });
+
+
+
+    it('should test the homePages controller', inject(function ($controller,
+            currentImposter, collectionItems, $rootScope, TPL_PATH, HEADER_LOCATION) {
 
         var parentScope = $rootScope.$new();
         var scope = parentScope.$new();
-        scope.currentImposter = {}; 
+        headerLocation = HEADER_LOCATION;
 
         var ctrl = $controller('HomeCtrl as home', {
-            $scope: scope
+            $scope: scope,
+            collectionItems: collectionItems,
+            currentImposter: currentImposter
         });
         expect(typeof scope.home.errorMessage).toEqual("string");
-        expect(scope.home.errorMessage).toEqual("No errors");
-        expect(scope.home.data.items[0].name).toEqual("Fred")
-        scope.home.data.items[0].name = "zipwit";
-        scope.home.inputChange();
-        var mapData = angular.fromJson(scope.home.displayData);
-        expect(mapData.items[0].name).toEqual("zipwit");
-        
-        
-    }));
+        expect(scope.home.errorMessage).toEqual("");
+        expect(scope.home.buffer.data.id).toEqual(1);
+        expect(scope.home.headerLocation).toEqual(headerLocation);
 
+
+    }));
+ 
+    it('test ImpostersService', inject(function ($controller,
+            currentImposter, collectionItems, $rootScope, TPL_PATH, HEADER_LOCATION) {
+        var parentScope = $rootScope.$new();
+        var scope = parentScope.$new();
+        headerLocation = HEADER_LOCATION;
+
+        var ctrl = $controller('HomeCtrl as home', {
+            $scope: scope,
+            collectionItems: collectionItems,
+            currentImposter: currentImposter
+        });
+        expect(typeof scope.home.addResponse).toEqual('function');
+
+    }));
    
 
 });
