3887c75c9012780322e2859e99629754826e1e6e
Don Henton <donhenton@gmail.com>
2018-03-20 07:14:34 -0500

    fix grunt file prepare for gh-pages

--- Gruntfile.js
+++ Gruntfile.js
@@ -149,7 +149,6 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-mountebank');
     grunt.loadNpmTasks('grunt-karma');
     grunt.registerTask('run-all-tests', ['clean', 'karma:unit_tests']);
     grunt.registerTask('build', ['clean', 'copy', 'concat', 'uglify', 'cssmin'])
